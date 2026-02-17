6164ede229a48088cac5d50ad81195ce463e1c7f
Biranté Soumaré <noricra.tetra@gmail.com>
2026-02-17 08:58:23 -0500

    Remove deprecated DB connection imports
    Removed deprecated database connection imports from __init__.py.

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,11 +1,7 @@
 from .settings import settings
 from .logging import configure_logging
-from .db import get_postgresql_connection, get_sqlite_connection
 
 __all__ = [
     "settings",
-    "configure_logging",
-    "get_postgresql_connection",
-    "get_sqlite_connection",  # DEPRECATED - Will be removed
 ]
 
